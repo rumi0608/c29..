5de5e034d2f34d408648d7ec5c1da5e625bcb58d
rumi0608 <79518215+rumi0608@users.noreply.github.com>
2021-04-28 18:16:13 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,42 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+         
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        i 
+        if(this.sling.bodyA){
+            
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            if (pointA.x<220){
+            line(pointA.x+20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+20, pointA.y, pointB.x+30, pointB.y);
+             
+            }
+            else{
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y);
+                 
+            }
+        }
+
+    }
+    
+}
+
